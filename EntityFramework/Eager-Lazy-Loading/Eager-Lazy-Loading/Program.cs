ba1b625384b55f5d5a7450e9cf96b1c07572530a
KishoreIthadi <ithadikishorebabu@gmail.com>
2017-12-14 20:48:30 +0530

    Added Entitty Framework Eager-Lazy loading sample

---
+++ EntityFramework/Eager-Lazy-Loading/Eager-Lazy-Loading/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Eager_Lazy_Loading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Eager_Lazy_LoadingEntities db = new Eager_Lazy_LoadingEntities())
+            {
+                List<tblDepartment> depList = db.tblDepartments.ToList();
+
+                foreach (var dep in depList)
+                {
+                    foreach (var emp in dep.tblEmployees)
+                    {
+                        Console.WriteLine(emp.Name);
+                    }
+                }
+            }
+        }
+    }
+}
