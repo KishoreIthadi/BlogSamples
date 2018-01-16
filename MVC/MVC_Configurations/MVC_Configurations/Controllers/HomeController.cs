dbef508b814f9fdaa099c45f00bcc59bb1b94162
KishoreIthadi <ithadikishorebabu@gmail.com>
2018-01-16 18:45:49 +0530

    Added Sample For "Publish MVC App With Different Configurations"

---
+++ MVC/MVC_Configurations/MVC_Configurations/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+
+namespace MVC_Configurations.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var conString = ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
+            ViewBag.ConString = conString;
+
+            return View();
+        }
+    }
+}
